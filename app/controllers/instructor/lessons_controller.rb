4636935f750579e1d456c60d4fde77116fb00d4a
Zac Jones <zacjones93@gmail.com>
2017-12-10 17:47:04 -0800

    finished permissions

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -9,19 +9,18 @@ class Instructor::LessonsController < ApplicationController
   def create
     @lesson = current_section.lessons.create(lesson_params)
     redirect_to instructor_course_path(current_section.course)
-  
   end
 
   private
 
   def require_authorized_for_current_section
-    if surrent_section.course.user != current_user
+    if current_section.course.user != current_user
       return render plain: 'Unauthorized', status: :unauthorized
     end
   end
 
   helper_method :current_section
-  private current_section
+  def current_section
    @current_section ||= Section.find(params[:section_id])
   end
 
